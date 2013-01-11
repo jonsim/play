d58e1312584b52814c3c59cadd60c5a7d9fd0de5
Jonathan Simmonds <js9721@bris.ac.uk>
2013-01-11 17:29:54 +0000

    Issued the project under the GPLv3 and CC-BY-SA licenses (see attached licenses for more information. Updated the readme.

--- Controls.java
+++ Controls.java
@@ -1,3 +1,32 @@
+/*-----------------------------------------------------------------------*/
+/*----------------------------    LICENSE    ----------------------------*/
+/*-----------------------------------------------------------------------*/
+/* This file is part of the Penguin Jump program (aka the play project)  */
+/* (https://github.com/rupert/play).                                     */
+/*                                                                       */
+/* Penguin Jump is free software: you can redistribute it and/or modify  */
+/* it under the terms of the GNU General Public License as published by  */
+/* the Free Software Foundation, either version 3 of the License, or     */
+/* (at your option) any later version.                                   */
+/*                                                                       */
+/* Penguin Jump is distributed in the hope that it will be useful,       */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of        */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         */
+/* GNU General Public License for more details.                          */
+/*                                                                       */
+/* You should have received a copy of the GNU General Public License     */
+/* along with Penguin Jump.  If not, see <http://www.gnu.org/licenses/>. */
+/*-----------------------------------------------------------------------*/
+
+
+/*-----------------------------------------------------------------------*/
+/*----------------------------     ABOUT     ----------------------------*/
+/*-----------------------------------------------------------------------*/
+/* Penguin Jump is a 1 or 2 player arcade style game written in Java,    */
+/* involving jumping between fish to reach the greatest height possible. */
+/* Authors: Jonathan Simmonds and Rupert Bedford                         */
+/*-----------------------------------------------------------------------*/
+
 import java.awt.event.*;
 
 class Controls implements KeyListener
---
+++ README.md
@@ -0,0 +1,20 @@
+Penguin Jump
+============
+
+Penguin Jump is a 1 or 2 player arcade style game written in Java, involving jumping between fish to 
+reach the greatest height possible.
+
+The project's code is released under the GPLv3 license while the project's graphical content is 
+released under the Creative Commons Attribution-ShareAlike 3.0 Unported License. It was created by 
+Jonathan Simmonds and Rupert Bedford.
+
+© Copyright 2010 Jonathan Simmonds & Rupert Bedford
+
+To compile and run:
+    javac Play.java
+    java Play
+    
+To compile, package and run:
+    javac Play.java
+    jar cfmv Play.jar Manifest.txt *.class images
+    java -jar Play.jar
